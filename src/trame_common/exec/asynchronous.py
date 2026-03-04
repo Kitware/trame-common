5cb43b78c12056e78ba68c5e5ad1da65184e4d0a
Corentin MUNOZ <corentin.munoz@kitware.com>
2026-03-04 17:08:18 +0100

    fix: create event loop if there is none

--- src/trame_common/exec/asynchronous.py
+++ src/trame_common/exec/asynchronous.py
@@ -46,7 +46,11 @@ def create_task(coroutine, loop=None):
     :rtype: asyncio.Task
     """
     if loop is None:
-        loop = asyncio.get_event_loop()
+        try:
+            loop = asyncio.get_event_loop()
+        except RuntimeError:
+            loop = asyncio.new_event_loop()
+            asyncio.set_event_loop(loop)
 
     return decorate_task(loop.create_task(coroutine))
 
