48f3669f93c921454ea46efd17e67930236e3c3d
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2026-07-31 17:11:40 -0600

    fix(TrameComponent): fully implement _unbind_annotated_methods

---
+++ tests/test_component.py
@@ -0,0 +1,159 @@
+import asyncio
+from contextlib import suppress
+
+import pytest
+from trame.app import TrameComponent, get_server
+from trame.decorators import change, controller, trigger
+from trame.ui.html import DivLayout
+from trame.widgets import html
+from trame_server.controller import FunctionNotImplementedError
+
+YIELD_TIME = 0.01
+
+
+@pytest.mark.asyncio
+async def test_state():
+    calls = []
+    server = get_server("test_state")
+    state = server.state
+    state.a = 1
+
+    class Dummy(TrameComponent):
+        def __init__(self, server, name):
+            super().__init__(server)
+            self._name = name
+
+        @change("a")
+        def _a_changed(self, a, **_):
+            calls.append(f"{self._name} = {a}")
+
+    first = Dummy(server, "first")
+    task = server.start(exec_mode="task", port=0)
+    await server.ready
+
+    assert calls == ["first = 1"]
+    with state:
+        state.a = 2
+    await asyncio.sleep(YIELD_TIME)
+    assert calls == ["first = 1", "first = 2"]
+
+    first._unbind_annotated_methods()
+    with state:
+        state.a = 3
+    await asyncio.sleep(YIELD_TIME)
+    assert calls == ["first = 1", "first = 2"]
+
+    second = Dummy(server, "second")
+    with state:
+        state.a = 4
+    await asyncio.sleep(YIELD_TIME)
+    assert calls == ["first = 1", "first = 2", "second = 4"]
+
+    second._unbind_annotated_methods()
+    with state:
+        state.a = 5
+    await asyncio.sleep(YIELD_TIME)
+    assert calls == ["first = 1", "first = 2", "second = 4"]
+
+    assert state.a == 5
+    await server.stop()
+    await task
+
+
+@pytest.mark.asyncio
+async def test_triggers():
+    calls = []
+    server = get_server("test_triggers")
+    ctrl = server.controller
+
+    class Dummy(TrameComponent):
+        def __init__(self, server, name):
+            super().__init__(server)
+            self._name = name
+
+        def click(self):
+            calls.append(f"{self._name} clicked")
+
+        @trigger("me")
+        def named_trigger(self):
+            calls.append(f"{self._name} named trigger")
+
+    def detached_method(): ...
+
+    assert len(ctrl._triggers) == 0
+    first = Dummy(server, "first")
+    assert len(ctrl._triggers) == 1
+
+    with DivLayout(server):
+        html.Button("Click Me", click=first.click)
+        html.Button("Click Me", click=ctrl.on_click)
+
+    assert len(ctrl._triggers) == 3
+
+    task = server.start(exec_mode="task", port=0)
+    await server.ready
+    # >>>
+    assert len(ctrl._triggers) == 3
+    ctrl.trigger("detach")(detached_method)
+    assert len(ctrl._triggers) == 4
+    ctrl.trigger_unregister(detached_method)
+    assert len(ctrl._triggers) == 3
+    first._unbind_annotated_methods()
+    assert len(ctrl._triggers) == 1
+    ctrl.trigger_unregister(ctrl.on_click)
+    assert len(ctrl._triggers) == 0
+    # <<<
+    await server.stop()
+    await task
+
+
+def test_ctrl():
+    calls = []
+    server = get_server("test_ctrl")
+    ctrl = server.controller
+
+    class Dummy(TrameComponent):
+        def __init__(self, server, name):
+            super().__init__(server)
+            self._name = name
+
+        @controller.add("me")
+        def method_add(self):
+            calls.append(f"{self._name} me")
+
+        @controller.once("yo")
+        def method_once(self):
+            calls.append(f"{self._name} yo")
+
+        @controller.set("hello")
+        def method_set(self):
+            calls.append(f"{self._name} hello")
+
+    first = Dummy(server, "first")
+    assert ctrl.me.exists()
+    assert ctrl.hello.exists()
+    assert not ctrl.empty.exists()
+
+    ctrl.yo.enable_empty()
+
+    ctrl.me()
+    ctrl.hello()
+    ctrl.me()
+    ctrl.yo()
+    ctrl.yo()
+
+    print(calls)
+    assert calls == ["first me", "first hello", "first me", "first yo"]
+
+    first._unbind_annotated_methods()
+
+    with suppress(FunctionNotImplementedError):
+        ctrl.hello()
+
+    with suppress(FunctionNotImplementedError):
+        ctrl.me()
+
+    assert calls == ["first me", "first hello", "first me", "first yo"]
+
+    assert not ctrl.me.exists()
+    assert not ctrl.hello.exists()
